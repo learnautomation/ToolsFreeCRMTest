23a8bc006e6d1281a938a483624d7513e266e42a
Sreekanth_Manikya <manikya.sreekanth@gmail.com>
2018-03-12 22:19:55 +0530

    added some code

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -1,5 +1,53 @@
 package com.crm.qa.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.crm.qa.util.TestUtil;
+
+
 public class TestBase {
 
+	static WebDriver driver;
+	static Properties prop; 
+	
+	public TestBase(){
+		
+		try{
+		prop = new Properties();
+		FileInputStream ip = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/java/com/crm/qa/config/Config.Properties");
+		prop.load(ip);
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		//System.out.println(browserName);		
+		if(browserName.equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Manikya\\Downloads\\SELENIUM\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if(browserName.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Manikya\\Downloads\\SELENIUM\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+	}
+	
+	
 }
--- src/main/java/com/crm/qa/pages/LoginPage.java
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -1,5 +1,26 @@
 package com.crm.qa.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+	
+	// Page Factory -OR;
+	
+	@FindBy(name = "username")
+	WebElement username;
+	
+	@FindBy(name = "password")
+	WebElement password;
+	
+	@FindBy(xpath = "//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath = "//button[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath = "//img[contains(@class, 'img-responsive']")
+	WebElement crmLogo;
 }
